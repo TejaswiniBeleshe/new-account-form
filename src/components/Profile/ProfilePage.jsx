c0a8a4495d46efa46b85227388b066e1ea6dae2d
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-02-27 23:55:58 +0530

    Validate inputs

--- src/components/Profile/ProfilePage.jsx
+++ src/components/Profile/ProfilePage.jsx
@@ -11,20 +11,20 @@ const ProfilePage = () => (
  
         // checl first name
         if(!values.fname || values.fname.trim() === ""){
-            errors.fname = "Required"
+            errors.fname = "First name Required"
         }else if(values.fname.length <= 2){
             errors.fname = "First name must be at least 4 characters"
         }
 
         // check last name
         if(!values.lname || values.fname.trim() === ""){
-            errors.fname = "Required"
+            errors.fname = "Last name Required"
         }
 
 
         // check email
         if (!values.email || values.email.trim() === "") {
-          errors.email = 'Required';
+          errors.email = 'Email Required';
         } else if (
           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
         ) {
@@ -33,7 +33,7 @@ const ProfilePage = () => (
         
         // check phone number
         if(!values.phoneNum || values.phoneNum.trim() === ""){
-            errors.phoneNum = "Required"
+            errors.phoneNum = "Phone number Required"
         }else if(!/^\d{10}$/.test(values.phoneNum)){
             // checking phone number contains exactly 10 digits
             errors.phoneNum = 'Invalid phone number'
@@ -41,7 +41,7 @@ const ProfilePage = () => (
         
         // check password
         if(!values.password || values.password.trim() === ""){
-            errors.password="Required"
+            errors.password="password Required"
         }else if(values.password.length<8){
             errors.password = "Password must be at least 8 characters"
         }
@@ -77,36 +77,63 @@ const ProfilePage = () => (
           <input 
             type="text" 
             name="fname"
+            placeholder='Input Your First Name'
             onChange={handleChange} 
             onBlur={handleBlur}
             value={values.fname}
         />
         <p>{errors.fname && touched.fname && errors.fname}</p>
+
         <input
             type="text"
             name="lname"
+            placeholder="Input Your Last Name"
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.lname}/>
         <p>{errors.lname && touched.lname && errors.lname}</p>
+
           <input
             type="email"
             name="email"
+            placeholder="Input Your Email"
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.email}
           />
           <p>{errors.email && touched.email && errors.email}</p>
-          <br/>
-          <input type="text" />
+          
+          <input
+           type="text"
+           name="phoneNum"
+           placeholder="Input Your Phone Number"
+           onChange={handleChange}
+           onBlur={handleBlur}
+           value={values.phoneNum}
+            />
+
+           <p>{errors.phoneNum && touched.phoneNum && errors.phoneNum}</p>
+
           <input
             type="password"
             name="password"
+            placeholder="Create Password"
             onChange={handleChange}
             onBlur={handleBlur}
             value={values.password}
           />
-          {errors.password && touched.password && errors.password}
+          <p>{errors.password && touched.password && errors.password}</p>
+
+          <input 
+          type="password"
+          name="conformpassword"
+          placeholder="Conform Your Password"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.conformPassword} />
+         <p>{errors.conformPasswordf && touched.conformPassword && errors.conformPassword}</p>
+
+
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
